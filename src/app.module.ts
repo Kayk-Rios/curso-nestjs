a3ae08051e2493d911f46e45771d883d736f13fd
Kayk_Darios <manjac688@gmail.com>
2025-04-28 02:13:26 -0300

    feat: inclusao do userService no appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [AuthModule, UserModule, DatabaseModule],
   controllers: [],
-  providers: [],
+  providers: [UserService],
 })
 export class AppModule {}
